c6aea2d0f2c6d82e27f9c8d85ec233e6efa58040
Pierre-Yves.Jacquier <pierre-yves.jacquier@telecomnancy.eu>
2023-10-16 09:32:57 +0200

    Ébauche de lecteur

---
+++ src/Lecteur.java
@@ -0,0 +1,55 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Lecteur {
+    private FileReader fileReader;
+    public char tete;
+    public String cheminFichier;
+    private static int nbLecteurs = 0;
+    private int numLecteur;
+
+    public Lecteur(String nomFichier) throws FileNotFoundException, IOException {
+    // Constructeur prenant comme argument le chemin vers un fichier sous la forme d'un string
+        try {
+            File fichier = new File(nomFichier);
+            this.fileReader = new FileReader(fichier);
+            this.tete = (char) fileReader.read();
+            this.cheminFichier = nomFichier;
+            this.numLecteur = nbLecteurs;
+            nbLecteurs++;
+        } catch (FileNotFoundException e) {
+            throw new FileNotFoundException(nomFichier + " introuvable");
+        }
+    }
+
+    public Lecteur(File fichier) throws FileNotFoundException, IOException {
+    // Constructeur prenant comme argument un fichier
+        try {
+            this.fileReader = new FileReader(fichier);
+            this.tete = (char) fileReader.read();
+            this.cheminFichier = fichier.getPath().toString();
+            this.numLecteur = nbLecteurs;
+            nbLecteurs++;
+        } catch (FileNotFoundException e) {
+            throw new FileNotFoundException(fichier.toString() + " introuvable");
+        }
+    }
+
+    public char lire() throws IOException {
+    // Avance d'un caractère et retourne sa valeur
+        int nextChar = fileReader.read();
+        if (nextChar == -1) {
+            System.out.println("AVERTISSEMENT : le lecteur "+this.toString()+" a atteint la fin de "+this.cheminFichier+" et ne peut plus le lire");
+            this.tete = '\0';
+        } else {
+            this.tete = (char) nextChar;
+        }
+        return this.tete;
+    }
+
+    public String toString() {
+        return "Lecteur " + String.valueOf(this.numLecteur);
+    }
+}
---
+++ src/TestLecteur.java
@@ -0,0 +1,14 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class TestLecteur {
+    public static void main(String[] args) throws FileNotFoundException, IOException{
+        Lecteur lecteur = new Lecteur("Programmes/salut.txt"); // Le chemin dépend d'où on est situé, en général on exécute depuis la racine du projet.
+        System.out.println(lecteur.tete); // Affiche "s" car c'est le premier caractère de "salut"
+        System.out.println(lecteur.lire()); // Afriche "a" car lire() avance la tête de 1
+        for (int i = 0; i < 4; i++) {
+            lecteur.lire(); // Sur la dernière itération, cette méthode affiche un avertissement, car la fin du fichier a été atteinte.
+            System.out.println(lecteur.tete);
+        }
+    }
+}
