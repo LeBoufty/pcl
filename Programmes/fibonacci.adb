18024f43081c7eca1a285fc29a9757ab12d5dc5b
Titouan <titouan.langlais@telecomnancy.eu>
2023-10-16 16:45:08 +0200

    Fibonacci récursive

---
+++ Programmes/fibonacci.adb
@@ -0,0 +1,25 @@
+with Ada.Text_IO; use Ada.Text_IO;
+
+procedure Fibonacci is
+    function Fib(n: Natural) return Natural is
+    begin
+        if n = 0 then
+            return 0;
+        elsif n = 1 then
+            return 1;
+        else
+            return Fib(n - 1) + Fib(n - 2);
+        end if;
+    end Fib;
+
+    --Variables
+n : Natural := 10;
+res : Natural := 0;
+
+    --Main
+begin
+    res := Fib(n);
+    Ada.Text_IO.Put("Fibonacci de " & n'Image);
+    Ada.Text_IO.Put(" est :" & res'Image);
+end Fibonacci;
+
