d4ab3c61a4785218bba8c040ac2d927d89ad455c
Pierre-Yves.Jacquier <pierre-yves.jacquier@telecomnancy.eu>
2023-11-08 11:46:00 +0100

    Fonctions

---
+++ src/arbres/Return.java
@@ -0,0 +1,11 @@
+package arbres;
+
+public class Return implements Noeud {
+    public Evaluable valeur;
+    public Return(Evaluable valeur) {
+        this.valeur = valeur;
+    }
+    public String toString() {
+        return "return " + this.valeur.toString() + ";";
+    }
+}
