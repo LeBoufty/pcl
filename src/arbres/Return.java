e26769985c30e58ed569ea482643022d3e58dd9d
Pierre-Yves.Jacquier <pierre-yves.jacquier@telecomnancy.eu>
2023-11-14 14:54:03 +0100

    Prise en compte de "return;"

--- src/arbres/Return.java
+++ src/arbres/Return.java
@@ -5,10 +5,20 @@ public class Return implements Noeud {
     public Return(Evaluable valeur) {
         this.valeur = valeur;
     }
+    public Return() {
+        this.valeur = null;
+    }
+    public boolean estVide() {
+        return this.valeur == null;
+    }
     public String toString() {
-        return "return " + this.valeur.toString() + ";";
+        if (!estVide()) {
+            return "return " + this.valeur.toString() + ";";
+        } else {
+            return "return;";
+        }
     }
     public boolean valide() {
-        return this.valeur.valide();
+        return this.estVide() || this.valeur.valide();
     }
 }
