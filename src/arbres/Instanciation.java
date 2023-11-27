3257303b4b6a3a1f344605bf65a6b0cff3aebd36
Pierre-Yves.Jacquier <pierre-yves.jacquier@telecomnancy.eu>
2023-11-27 08:32:54 +0100

    Noms de variables

--- src/arbres/Instanciation.java
+++ src/arbres/Instanciation.java
@@ -3,7 +3,9 @@ package arbres;
 public class Instanciation implements Noeud {
     public Type type;
     public Variable variable;
-    public Instanciation(Type t) {type = t; variable = new Variable(t);}
+    public String nom;
+    public Instanciation(Type t) {type = t; variable = new Variable(t); nom = Repertoire.createID(this);}
+    public Instanciation(Type t, String nom) {type = t; variable = new Variable(t, nom); this.nom = nom;}
     public String toString() {
         return variable.toString() + " : " + type.toString() + ";";
     }
