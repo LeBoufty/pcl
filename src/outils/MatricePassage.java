e4ea7cc9a6a5e4efb5a820f5f852322da1f9de5d
KRAFFT Arnaud <arnaud.krafft@telecomnancy.eu>
2023-12-13 15:11:15 +0100

    prep dico+fct

---
+++ src/outils/MatricePassage.java
@@ -0,0 +1,7 @@
+package outils;
+//Madpad4lyfe
+public class MatricePassage {
+    //fonction qui prend en entrée l'entier indentificateur du dico lexi vers l'autre. Il sort un Array[]
+    //pour le 53 l'array est de taille 3 du coup caractere, ' et val
+    //fonction qui fai tl'inverse
+}
