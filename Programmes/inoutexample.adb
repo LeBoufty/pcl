7daa6c799dbde60e6a74442b41dfecfe39e01179
Titouan <titouan.langlais@telecomnancy.eu>
2023-12-20 11:11:02 +0100

    Add InOutExample program with procedure using in out parameter

---
+++ Programmes/inoutexample.adb
@@ -0,0 +1,23 @@
+with Ada.Text_IO; use Ada.Text_IO;
+
+procedure InOutExample is
+   -- Procedure with in out parameter
+   procedure ModifyNumber(InOutParam : in out Integer) is
+   begin
+      InOutParam := InOutParam * 2;
+   end ModifyNumber;
+
+   -- Main program
+   Number : Integer := 10;
+
+begin
+   Put("Original number: ");
+   Put(Number'Image);
+   New_Line;
+
+   ModifyNumber(Number);  -- Calling the procedure with in out parameter
+
+   Put("Modified number: ");
+   Put(Number'Image);
+   New_Line;
+end InOutExample;
