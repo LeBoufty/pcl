6b63561cf0589fdc6e63e06f15204de704b16909
Pierre-Yves.Jacquier <pierre-yves.jacquier@telecomnancy.eu>
2023-12-21 14:11:42 +0100

    Fonction put dans l'arbre

--- src/tests/TestArbreUnDebutTopDown.java
+++ src/tests/TestArbreUnDebutTopDown.java
@@ -6,6 +6,17 @@ import outils.Logger;
 
 public class TestArbreUnDebutTopDown {
     public static void main(String[] args) {
+        ////////////////////////////////
+        // Sera réalisé à l'initalisation de l'analyseur syntaxique
+        Instanciation nToPut = new Instanciation(Type.INTEGER, "n");
+        Instanciation cToPut = new Instanciation(Type.CHARACTER, "c");
+        Fonction putInt = new Fonction("put");
+        putInt.params.add(nToPut);
+        putInt.type = Type.NULLTYPE;
+        Fonction putChar = new Fonction("put");
+        putChar.params.add(cToPut);
+        putChar.type = Type.NULLTYPE;
+        ////////////////////////////////
         // Début
         Procedure unDebut = new Procedure("unDebut");
         // aireRectangle :
@@ -73,12 +84,18 @@ public class TestArbreUnDebutTopDown {
         ((AppelFonction)thenAffectation.droite).ajouterParametre(new Constante(2));
         ((AppelFonction)thenAffectation.droite).ajouterParametre(new Constante(3));
         choixegal1.ajouterInstructionAlors(thenAffectation);
+        AppelFonction putValeurPerim = new AppelFonction(putInt);
+        putValeurPerim.ajouterParametre(valeur.variable);
+        choixegal1.ajouterInstructionAlors(putValeurPerim);
         Affectation elseAffectation = new Affectation();
         elseAffectation.gauche = valeur.variable;
         elseAffectation.droite = new AppelFonction(aireRectangle);
         ((AppelFonction)elseAffectation.droite).ajouterParametre(new Constante(2));
         ((AppelFonction)elseAffectation.droite).ajouterParametre(new Constante(3));
         choixegal1.ajouterInstructionSinon(elseAffectation);
+        AppelFonction putValeurAire = new AppelFonction(putInt);
+        putValeurAire.ajouterParametre(valeur.variable);
+        choixegal1.ajouterInstructionSinon(putValeurAire);
         unDebut.ajouterInstruction(choixegal1);
 
         // Impression
