0426e95237c9ff1979ae977838e7b1eebcfabc2e
Titouan <titouan.langlais@telecomnancy.eu>
2023-12-27 21:17:55 +0100

    Refactor clean_text function to handle spaces

--- src/grammaire/parseur_grammaire.py
+++ src/grammaire/parseur_grammaire.py
@@ -7,20 +7,25 @@ def extract_tables_from_html(html_file):
         tables = soup.find_all('table')
         return tables
 
-def clean_text(text):
+def clean_text(cell):
+    text = cell.get_text(strip=False)
+    text = text.replace(' ', '&')
     text = text.replace(',', 'virgule')
     text = text.replace('ε', '€')
-    if not text.strip():
-        text = '§'
+
     if '→' in text:
         text = text.split('→')[1].strip()
+
+    if not text:
+        text = '§'
+
     return text
 
 def convert_table_to_csv(table, csv_file):
     with open(csv_file, 'w', newline='', encoding='utf-8') as csvfile:
         csv_writer = csv.writer(csvfile)
         for row in table.find_all('tr'):
-            csv_writer.writerow([clean_text(col.get_text(strip=True)) for col in row.find_all(['th', 'td'])])
+            csv_writer.writerow([clean_text(col) for col in row.find_all(['th', 'td'])])
 
 def main():
     html_file = 'src/grammaire/Grammophone.html'
