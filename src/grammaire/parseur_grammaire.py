09c0ae73d6fa8d3f1810accba059c8b14ecb8484
Titouan <titouan.langlais@telecomnancy.eu>
2023-12-27 21:34:59 +0100

    Refactor clean_text function and add header row check in convert_table_to_csv

--- src/grammaire/parseur_grammaire.py
+++ src/grammaire/parseur_grammaire.py
@@ -7,16 +7,25 @@ def extract_tables_from_html(html_file):
         tables = soup.find_all('table')
         return tables
 
-def clean_text(cell):
+def clean_text(cell, header_row):
     text = cell.get_text(strip=False)
     text = text.replace(',', 'virgule')
-    text = text.replace('ε', '€')
 
     if '→' in text:
         text = text.split('→')[1].strip()
 
+    if 'ε' in text:
+        return '€'
+
     if not text:
-        text = '§'
+        return '§'
+
+    words = text.split()
+    for i, word in enumerate(words):
+        if word not in header_row:
+            words[i] = f'£{word}'
+
+    text = ' '.join(words)
 
     text = ' '.join('&'.join(word.strip() for word in text.split()) for text in text.split('&'))
 
@@ -25,8 +34,9 @@ def clean_text(cell):
 def convert_table_to_csv(table, csv_file):
     with open(csv_file, 'w', newline='', encoding='utf-8') as csvfile:
         csv_writer = csv.writer(csvfile)
-        for row in table.find_all('tr'):
-            csv_writer.writerow([clean_text(col) for col in row.find_all(['th', 'td'])])
+        header_row = [col.get_text(strip=True) for col in table.find('tr').find_all(['th', 'td'])]
+        for row in table.find_all('tr')[1:]:
+            csv_writer.writerow([clean_text(col, header_row) for col in row.find_all(['th', 'td'])])
 
 def main():
     html_file = 'src/grammaire/Grammophone.html'
