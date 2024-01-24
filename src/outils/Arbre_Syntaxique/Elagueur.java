6c6bbc49789b56750ad9583c7c02a6889e6ac0ef
KRAFFT Arnaud <arnaud.krafft@telecomnancy.eu>
2024-01-24 15:31:00 +0100

    annulade

--- src/outils/Arbre_Syntaxique/Elagueur.java
+++ src/outils/Arbre_Syntaxique/Elagueur.java
@@ -1,7 +1,6 @@
 package outils.Arbre_Syntaxique;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Stack;
@@ -146,14 +145,9 @@ public class Elagueur {
         while (!pile.isEmpty()) {
             Noeud_A noeud = pile.pop();
             if (noeud instanceof Noeud_Non_Terminal) {
-                List<Noeud_A> enfants = ((Noeud_Non_Terminal) noeud).getEnfants();
-                Collections.reverse(enfants);
-                for (Noeud_A enfant : enfants) {
-                pile.push(enfant);
+                for (Noeud_A enfant : ((Noeud_Non_Terminal) noeud).getEnfants()) {
+                    pile.push(enfant);
                 }
-                // for (Noeud_A enfant : ((Noeud_Non_Terminal) noeud).getEnfants()) {
-                //     pile.push(enfant);
-                // }
                 if (estEtoile((Noeud_Non_Terminal)noeud)) {
                     tag.add((Noeud_Non_Terminal)noeud);
                 }
