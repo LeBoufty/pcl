c562e7b5143f66692c95b07626cfa54cccb9cfc3
Titouan <titouan.langlais@telecomnancy.eu>
2024-01-24 15:21:57 +0100

    Added somes lines quelquesintructions

--- Programmes/Fonctionne/quelquesinstructions.adb
+++ Programmes/Fonctionne/quelquesinstructions.adb
@@ -6,7 +6,11 @@ procedure Instructions is
     sortie: Boolean;
 begin
     a := -4;
+    a;
     b := a + 1;
+    b := 1-1+1;
+    b := 1*2+3*(4+5);
+    b := (1-1+1*2*(1+(1*2)));
     if b > 0 then
         sortie := true;
     else
