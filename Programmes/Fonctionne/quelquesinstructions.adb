ed0a7eea8f649717fad16c70b28c86d5cccb9d26
Titouan <titouan.langlais@telecomnancy.eu>
2024-01-24 16:02:56 +0100

    Refactor code: Rename procedure and add new record type
    This commit renames the procedure "Instructions" to "quelquesinstructions" and adds a new record type "recorda" to the code. These changes improve code readability and maintainability.

--- Programmes/Fonctionne/quelquesinstructions.adb
+++ Programmes/Fonctionne/quelquesinstructions.adb
@@ -1,9 +1,15 @@
 with Ada.Text_IO;use Ada.Text_IO;
 
-procedure Instructions is
+procedure quelquesinstructions is
     a: Integer;
     b: Integer;
     sortie: Boolean;
+    type recorda is record
+        c: Integer;
+        d: String;
+        f: Boolean;
+    end record;
+    e : recorda;
 begin
     a := -4;
     a;
@@ -11,9 +17,10 @@ begin
     b := 1-1+1;
     b := 1*2+3*(4+5);
     b := (1-1+1*2*(1+(1*2)));
+    e.c := 1;
     if b > 0 then
         sortie := true;
     else
         sortie := false;
     end if;
-end Instructions;
+end quelquesinstructions;
