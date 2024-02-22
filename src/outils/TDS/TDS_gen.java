cbcddd66d4ff78e692bd263ed17c35596d23180c
KRAFFT Arnaud <arnaud.krafft@telecomnancy.eu>
2024-02-22 13:12:18 +0100

    nouvelle TDS rennomé

--- src/outils/TDS/TDS_gen.java
+++ src/outils/TDS/TDS_gen.java
@@ -4,32 +4,33 @@ import java.util.ArrayList;
 
 import arbres.Noeud;
 
-public class Struct_TDS2 {
+public class TDS_gen {
     //doit contenir num imbr, num reg, nom de fonction
     //mettre nom deplacement taille pour chaque variable
+    //doit pouvoir donner le père
     
-    public ArrayList<Struct_TDS2> tds_childrens;
-    public Struct_TDS2 tds_parent;
+    public ArrayList<TDS_gen> tds_childrens;
+    public TDS_gen tds_parent;
     public Noeud noeud_associé;
     public int tds_depth;
 
     public ArrayList<Integer> tds_code;
 
-    public Struct_TDS2(Noeud noeud_associé) {
+    public TDS_gen(Noeud noeud_associé) {
         this.noeud_associé = noeud_associé;
-        this.tds_childrens = new ArrayList<Struct_TDS2>();
+        this.tds_childrens = new ArrayList<TDS_gen>();
         this.tds_code = new ArrayList<Integer>();
         this.tds_parent = null;
         this.tds_depth = 0;
     }
 
-    public void add_TDS_child(Struct_TDS2 child) {
+    public void add_TDS_child(TDS_gen child) {
         this.tds_childrens.add(child);
         child.tds_parent = this;
         child.tds_depth = this.tds_depth + 1;
     }
 
-    public void remove_TDS_child(Struct_TDS2 child) {
+    public void remove_TDS_child(TDS_gen child) {
         this.tds_childrens.remove(child);
         child.tds_parent = null;
         child.tds_depth = 0;
