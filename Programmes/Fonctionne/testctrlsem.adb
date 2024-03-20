2f2c172bfd2d5efd1178477e874db31961b61d50
KRAFFT Arnaud <arnaud.krafft@telecomnancy.eu>
2024-03-20 17:15:46 +0100

    mod fich test

--- Programmes/Fonctionne/testctrlsem.adb
+++ Programmes/Fonctionne/testctrlsem.adb
@@ -17,20 +17,4 @@ begin
     c := 2;
     e := 7;
     a := -4;
-    e.c := 1;
-    e.f := a>=b=e.c;
-    e.d := a + b;
-    while a < 10 loop
-        a := a + 1;
-    end loop;
-    if b and b and then c or a or else 1 > 0 then
-        sortie := true;
-        lol := 1;
-    elsif b and b then
-        sortie := true;
-    elsif b and b and then c or a or else 1 > 0 then
-        sortie := true;
-    else
-        sortie := false;
-    end if;
 end quelquesinstructions;
