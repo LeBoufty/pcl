ef9ed413987d06010379e60a16400e5c40832836
Titouan LANGLAIS <titouan.langlais@telecomnancy.eu>
2024-03-29 11:22:02 +0100

    Refactor Pile_registre class to use private static variables and fix empiler() and depiler() methods

--- src/outils/Pile_registre/Pile_registre.java
+++ src/outils/Pile_registre/Pile_registre.java
@@ -1,57 +1,35 @@
 package outils.Pile_registre;
 
 public class Pile_registre {
-    public int taille;
-    public int[] pile;
-    public int sommet;
+    private static int[] pile;
+    private static int sommet;
 
     public Pile_registre(int taille) {
-        this.taille = taille;
-        this.pile = new int[taille];
-        this.sommet = 0;
+        pile = new int[taille];
+        sommet = -1;
     }
 
     public void empiler(int valeur) {
-        if (this.sommet < this.taille) {
-            this.pile[this.sommet] = valeur;
-            this.sommet++;
-        } else {
-            System.out.println("Pile pleine");
-        }
+        pile[++sommet] = valeur;
     }
 
     public int depiler() {
-        if (this.sommet > 0) {
-            this.sommet--;
-            return this.pile[this.sommet];
-        } else {
-            System.out.println("Pile vide");
-            return -1;
-        }
-    }
-
-    public int sommet() {
-        if (this.sommet > 0) {
-            return this.pile[this.sommet - 1];
-        } else {
-            System.out.println("Pile vide");
-            return -1;
-        }
+        return pile[sommet--];
     }
 
     public void afficher() {
-        System.out.print("Pile : ");
-        for (int i = 0; i < this.sommet; i++) {
-            System.out.print(this.pile[i] + " ");
+        for (int i = 0; i <= sommet; i++) {
+            System.out.print(pile[i] + " ");
         }
         System.out.println();
     }
-    
+
     public static void init() {
-        Pile_registre pile = new Pile_registre(10);
-        int[] registres = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] registres = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};// TODO : Registres à initialiser
+        Pile_registre pile = new Pile_registre(registres.length); 
         for (int i = 0; i < registres.length; i++) {
             pile.empiler(registres[i]);
         }
+        pile.afficher();
     }
 }
