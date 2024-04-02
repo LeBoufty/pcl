e1d63fa25e2ae8126f3f1f4b79e9d2d3093ef9c7
Pierre-Yves.Jacquier <pierre-yves.jacquier@telecomnancy.eu>
2024-04-02 14:30:52 +0200

    Ajout problème 5

---
+++ Programmes/testsem/erreur5.adb
@@ -0,0 +1,12 @@
+with Ada.Text_IO;use Ada.Text_IO;
+
+procedure erreur5 is
+    a: Integer;
+    b: Integer;
+    sortie: Boolean;
+
+begin
+    a := 21;
+    b := a + w;
+    a := -4;
+end erreur5;
