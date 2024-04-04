eaa0f9ab78087d925986c53650611ff324ac9ed4
Titouan LANGLAIS <titouan.langlais@telecomnancy.eu>
2024-04-04 16:18:24 +0200

    Fix typo car je suis un boloss

--- src/arbres/Procedure.java
+++ src/arbres/Procedure.java
@@ -98,7 +98,7 @@ public class Procedure implements Noeud {
             GestionFichier.AddcontenuHeader(".global F"+ this.tds.get_num_reg() +"\n.extern printf // Import printf\n.section .data\n");
             GestionFichier.Addcontenu(".section .text\nF"+ this.tds.get_num_reg() +" :\n");
             GestionFichier.AddcontenuFooter("bl exit\n\nexit : //Fonction de sortie du programme \nmov x0,#0\nmov x8,#93\nsvc #0\nret\n");
-            GestionFichier.AddcontenuFooter("\nget_global_var: ADD BP, BP, #8 // On passe à la variable suivante, x0 depl, x1 nb_saut\nSUBS x1, x1, #1 // On décrémente le nombre de saut\nBNE get_global_var // On boucle tant que x1 != 0\nLDR x0, [BP, x0] // On charge la valeur de la variable\nRET\n");
+            GestionFichier.AddcontenuFooter("\nget_global_var : ADD BP, BP, #8 // On passe à la variable suivante, x0 depl, x1 nb_saut\nSUBS x1, x1, #1 // On décrémente le nombre de saut\nBNE get_global_var // On boucle tant que x1 != 0\nLDR x0, [BP, x0] // On charge la valeur de la variable\nRET\n");
         } else {
             res += "F"+this.tds.get_num_reg() + " :\n";
             res += "STP LR, X11, [SP, #-16]! // Sauvegarde LR\n";
