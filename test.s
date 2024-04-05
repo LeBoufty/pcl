065640d7caf1a715454042ca6391b73351417c7d
Titouan LANGLAIS <titouan.langlais@telecomnancy.eu>
2024-04-05 14:18:04 +0200

    :kite: !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- test.s
+++ test.s
@@ -3,7 +3,50 @@
 .section .data
 
 .section .text
-unDebut:
+F0 :
+
+// Declaration de la variable a
+SUB sp, sp, #8 // Allocation de 8 octets pour la variable a
+
+// Declaration de la variable y
+MOVZ x0, #100 // On met la constante en pile 
+SUB sp, sp, #8 // On décrémente le pointeur de pile 
+STR x0, [sp] // On met la constante en pile 
+
+// Declaration de la variable z
+SUB sp, sp, #8 // Allocation de 8 octets pour la variable z
+
+// Instructions de la procédure addition1
+MOVZ x0, #5 // On met la constante en pile 
+SUB sp, sp, #8 // On décrémente le pointeur de pile 
+STR x0, [sp] // On met la constante en pile 
+LDR x2, [sp] // On met la valeur de la variable droite dans x0 
+STR x2, [x29, #-5] // On met la valeur de la variable droite dans la variable gauche 
+
+MOVZ x0, #3 // On met la constante en pile 
+SUB sp, sp, #8 // On décrémente le pointeur de pile 
+STR x0, [sp] // On met la constante en pile 
+LDR x2, [sp] // On met la valeur de la variable droite dans x0 
+STR x2, [x29, #-4] // On met la valeur de la variable droite dans la variable gauche 
+
+
+// Opération
+MOVZ x0, #3 // On met la constante en pile 
+SUB sp, sp, #8 // On décrémente le pointeur de pile 
+STR x0, [sp] // On met la constante en pile 
+MOVZ x0, #4 // On met la constante en pile 
+SUB sp, sp, #8 // On décrémente le pointeur de pile 
+STR x0, [sp] // On met la constante en pile 
+LDR x1, [sp] // On met l'opérande droite dans x1
+ADD sp, sp, #8 // On décrémente le pointeur de pile
+LDR x0, [sp] // On met l'opérande gauche dans x0
+ADD sp, sp, #8 // On décrémente le pointeur de pile
+ADD x0, x0, x1 // On effectue l'opération
+SUB sp, sp, #8 // On décrémente le pointeur de pile
+STR x0, [sp] // On met le résultat en pile
+LDR x2, [sp] // On met la valeur de la variable droite dans x0 
+STR x2, [x29, #-3] // On met la valeur de la variable droite dans la variable gauche 
+
 
 bl exit
 
@@ -13,21 +56,18 @@ mov x8,#93
 svc #0
 ret
 
-aireRectangle :STP LR, X11, [SP, #-16]!
-STP X10, XZR, [SP, #-16]!
-SUB SP, SP, taille_locale
-MOV x0, x0
-ADD SP, SP, taille_locale
-LDP X10, XZR, [SP], #16
-LDP LR, X11, [SP], #16
+
+get_global_var : ADD fp, fp, #8 // On passe à la variable suivante, x0 depl, x1 nb_saut
+SUBS x1, x1, #1 // On décrémente le nombre de saut
+BNE get_global_var // On boucle tant que x1 != 0
+LDR x0, [fp, x0] // On charge la valeur de la variable
+SUB sp,sp, #8 // On fait de la place dans la pile pour le retour
+STR x0, [sp] // On met la valeur de la variable en pile
 RET
 
-perimetreRectangle :STP LR, X11, [SP, #-16]!
-STP X10, XZR, [SP, #-16]!
-SUB SP, SP, taille_locale
-MOV x0, x0
-ADD SP, SP, taille_locale
-LDP X10, XZR, [SP], #16
-LDP LR, X11, [SP], #16
+set_global_var : ADD fp, fp, #8 // On passe à la variable suivante, x0 depl, x1 nb_saut
+SUBS x1, x1, #1 // On décrémente le nombre de saut
+BNE set_global_var // On boucle tant que x1 != 0
+STR x2, [fp, x0] // On charge la valeur de la variable
 RET
 
