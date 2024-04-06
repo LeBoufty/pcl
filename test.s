2d6f1b836bf5102ca92930943cbe76e1462d6c25
Titouan LANGLAIS <titouan.langlais@telecomnancy.eu>
2024-04-07 01:20:01 +0200

    Just a test before a beautiful night, alone, but with my bestie :flushed:

--- test.s
+++ test.s
@@ -10,18 +10,16 @@ main :
 STP x29, lr, [sp, #-16]! // Sauvegarde du pointeur de pile et du lien de retour
 MOV x29, sp // Mise à jour du pointeur de pile
 
-// Declaration de la variable a
-SUB sp, sp, #16 // Allocation de 8 octets pour la variable a
+// Declaration de la variable c
+SUB sp, sp, #16 // Allocation de 8 octets pour la variable c
 
 // Declaration de la variable y
-MOVZ x0, #100
-SUB sp, sp, #16 // On décrémente le pointeur de pile 
-STR x0, [sp] // On met la constante en pile 
+SUB sp, sp, #16 // Allocation de 8 octets pour la variable y
 
 // Declaration de la variable z
 SUB sp, sp, #16 // Allocation de 8 octets pour la variable z
 
-// Instructions de la procédure addition1
+// Instructions de la procédure addition3
 MOVZ x0, #5
 SUB sp, sp, #16 // On décrémente le pointeur de pile 
 STR x0, [sp] // On met la constante en pile 
@@ -38,12 +36,12 @@ ADD sp, sp, #16 // On dépile la valeur
 
 
 // Opération
-MOVZ x0, #6
+MOVZ x0, #1
 SUB sp, sp, #16 // On décrémente le pointeur de pile 
 STR x0, [sp] // On met la constante en pile 
-MOVZ x0, #4
-SUB sp, sp, #16 // On décrémente le pointeur de pile 
-STR x0, [sp] // On met la constante en pile 
+LDR x0, [x29, #-40] // z Mise en pile var
+SUB sp, sp, #16 // z Mise en pile var
+STR x0, [sp] // z Mise en pile var
 LDR x1, [sp] // On met l'opérande droite dans x1
 ADD sp, sp, #16 // On décrémente le pointeur de pile
 LDR x0, [sp] // On met l'opérande gauche dans x0
@@ -54,36 +52,6 @@ STR x0, [sp] // On met le résultat en pile
 LDR x2, [sp] // On met la valeur de la variable droite dans x0 
 STR x2, [x29, #-24] // On met la valeur de la variable droite dans la variable gauche 
 ADD sp, sp, #16 // On dépile la valeur 
-
-// Printf
-LDR x0, [x29, #-40] // z Mise en pile var
-SUB sp, sp, #16 // z Mise en pile var
-STR x0, [sp] // z Mise en pile var
-MOV x1, x0
-ADRP x0, format
-ADD x0, x0, :lo12:format
-BL printf
-ADD sp, sp, #16
-
-// Printf
-LDR x0, [x29, #-32] // y Mise en pile var
-SUB sp, sp, #16 // y Mise en pile var
-STR x0, [sp] // y Mise en pile var
-MOV x1, x0
-ADRP x0, format
-ADD x0, x0, :lo12:format
-BL printf
-ADD sp, sp, #16
-
-// Printf
-LDR x0, [x29, #-24] // a Mise en pile var
-SUB sp, sp, #16 // a Mise en pile var
-STR x0, [sp] // a Mise en pile var
-MOV x1, x0
-ADRP x0, format
-ADD x0, x0, :lo12:format
-BL printf
-ADD sp, sp, #16
 
 
 bl exit_program
