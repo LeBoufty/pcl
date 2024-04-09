8aa9e32fc075eaf1b2bb38956d55537fba60da5c
Titouan LANGLAIS <titouan.langlais@telecomnancy.eu>
2024-04-09 23:09:26 +0200

    Testing

--- Programmes/testAdasimples/Addition1.s
+++ Programmes/testAdasimples/Addition1.s
@@ -13,7 +13,9 @@ STP x29, lr, [sp, #-16]! // Sauvegarde du pointeur de pile et du lien de retour
 MOV x29, sp // Mise à jour du pointeur de pile
 
 // Declaration de la variable a
-SUB sp, sp, #16 // Allocation de 16 octets pour la variable a
+MOVZ x0, #90
+SUB sp, sp, #16 // On décrémente le pointeur de pile 
+STR x0, [sp] // On met la constante en pile 
 
 // Declaration de la variable y
 MOVZ x0, #100
@@ -21,7 +23,9 @@ SUB sp, sp, #16 // On décrémente le pointeur de pile
 STR x0, [sp] // On met la constante en pile 
 
 // Declaration de la variable z
-SUB sp, sp, #16 // Allocation de 16 octets pour la variable z
+MOVZ x0, #600000
+SUB sp, sp, #16 // On décrémente le pointeur de pile 
+STR x0, [sp] // On met la constante en pile 
 
 // Instructions de la procédure addition1
 MOVZ x0, #5
@@ -31,7 +35,7 @@ LDR x2, [sp] // On met la valeur de la variable droite dans x0
 STR x2, [x29, #-40] // On met la valeur de la variable droite dans la variable gauche 
 ADD sp, sp, #16 // On dépile la valeur 
 
-MOVZ x0, #3
+MOVZ x0, #60001
 SUB sp, sp, #16 // On décrémente le pointeur de pile 
 STR x0, [sp] // On met la constante en pile 
 LDR x2, [sp] // On met la valeur de la variable droite dans x0 
@@ -40,7 +44,17 @@ ADD sp, sp, #16 // On dépile la valeur
 
 
 // Opération
-MOVZ x0, #10 // On met le résultat de l'opération en x0
+LDR x0, [x29, #-32] // y Mise en pile var
+SUB sp, sp, #16 // y Mise en pile var
+STR x0, [sp] // y Mise en pile var
+MOVZ x0, #4
+SUB sp, sp, #16 // On décrémente le pointeur de pile 
+STR x0, [sp] // On met la constante en pile 
+LDR x1, [sp] // On met l'opérande droite dans x1
+ADD sp, sp, #16 // On décrémente le pointeur de pile
+LDR x0, [sp] // On met l'opérande gauche dans x0
+ADD sp, sp, #16 // On décrémente le pointeur de pile
+ADD x0, x0, x1 // Opération +
 SUB sp, sp, #16 // On décrémente le pointeur de pile
 STR x0, [sp] // On met le résultat en pile
 LDR x2, [sp] // On met la valeur de la variable droite dans x0 
