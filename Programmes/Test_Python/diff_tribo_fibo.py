78060d32b12c8d8feda38c88044f4465790e3317
KRAFFT Arnaud <arnaud.krafft@telecomnancy.eu>
2024-05-04 11:39:25 +0200

    fct test version modifiée

--- Programmes/Test_Python/diff_tribo_fibo.py
+++ Programmes/Test_Python/diff_tribo_fibo.py
@@ -1,10 +1,10 @@
 def diff_fib_trib(n):
-
-    def fibonacci(n):
-        if n <= 1:
-            return n
+    
+    def fibonacci(n, x1,x2):
+        if n == 0:
+            return x1
         else:
-            return fibonacci(n-1) + fibonacci(n-2)
+            return fibonacci(n-1,x2,x1+x2)
         
     def tribonacci(n):
         if n == 0:
@@ -14,11 +14,22 @@ def diff_fib_trib(n):
         else:
             return tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3)
     
+    def get_acceleration(f0,f1,f2,n1,n2):
+        return (f2-2*f1+f0)/((n2-n1)*(n-n1))
+
     tri = tribonacci(n)
-    fib = fibonacci(n)
+    fib = fibonacci(n,0,1)
     print("Tribonacci: ", tri)
     print("Fibonacci: ", fib)
     print("Difference: ", tri - fib)
+    if(n>1):
+        f0=tribonacci(n-2)-fibonacci(n-2,0,1)
+        f1=tribonacci(n-1)-fibonacci(n-1,0,1)
+        f2=tri-fib
+        print("Acceleration: ", get_acceleration(f0,f1,f2,n-1,n))
 
-n=7
+
+n=6
 diff_fib_trib(n)
+diff_fib_trib(n+1)
+diff_fib_trib(n+2)
